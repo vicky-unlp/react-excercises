ef47379b2feb9c2f573e243cdec5d2e5c0e9cb34
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-05-11 18:31:58 -0300

    Move folders to UI

---
+++ UI/rendering-lists/splitting-lists/src/App.js
@@ -0,0 +1,79 @@
+// Change it to show two separate lists one after another: Chemists and Everyone Else. 
+// Like previously, you can determine whether a person is a chemist by checking if 
+// person.profession === 'chemist'.
+
+
+// Original:
+
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
+
+// export default function List() {
+//   const listItems = people.map(person =>
+//     <li key={person.id}>
+//       <img
+//         src={getImageUrl(person)}
+//         alt={person.name}
+//       />
+//       <p>
+//         <b>{person.name}:</b>
+//         {' ' + person.profession + ' '}
+//         known for {person.accomplishment}
+//       </p>
+//     </li>
+//   );
+//   return (
+//     <article>
+//       <h1>Scientists</h1>
+//       <ul>{listItems}</ul>
+//     </article>
+//   );
+// }
+
+// Solution:
+
+export default function List() {
+  const chemists = people.filter(person =>
+    person.profession === 'chemist'
+  );
+  const everyoneElse = people.filter(person =>
+    person.profession !== 'chemist'
+  );
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <h2>Chemists</h2>
+      <ul>
+        {chemists.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+      <h2>Everyone Else</h2>
+      <ul>
+        {everyoneElse.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+    </article>
+  );
+}
